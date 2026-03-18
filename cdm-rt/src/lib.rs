ccaccec580003b7d8eef36501e2fee650d1acff4
aelsi2 <eliseevandrey2005@gmail.com>
2026-03-18 13:43:12 +0700

    Add doc comments to cdm-rt

--- cdm-rt/src/lib.rs
+++ cdm-rt/src/lib.rs
@@ -1,17 +1,49 @@
 #![no_std]
 #![feature(asm_experimental_arch)]
 
+/// The number of exception vectors in the interrupt vector table.
 pub const EXCEPTION_COUNT: usize = 4;
+
+/// The index of the first application-specific interrupt vector.
+pub const INTERRUPT_START: usize = EXCEPTION_COUNT + 1;
+
+/// The number of application-specific interrupt vectors in the interrupt vector table.
 pub const INTERRUPT_COUNT: usize = 59;
 
+/// Represents a vector in the interrupt vector table.
+///
+/// The first field is the pointer to the handler function.
+/// The second field is the initial value of the status register (usually 0 to disable interrupts).
 #[derive(Clone, Copy)]
 #[repr(C)]
-pub struct ExceptionVector(unsafe extern "C" fn() -> !, u16);
+pub struct InterruptVector(pub unsafe extern "cdm-isr" fn(), pub u16);
 
-#[derive(Clone, Copy)]
-#[repr(C)]
-pub struct InterruptVector(unsafe extern "cdm-isr" fn(), u16);
+impl InterruptVector {
+    /// The default vector used in absence of an explicit definition.
+    ///
+    /// Calls `InterruptHandler`, which triggers a hardware halt by default.
+    pub const DEFAULT: Self = {
+        unsafe extern "cdm-isr" {
+            fn InterruptHandler();
+        }
+        InterruptVector(InterruptHandler, 0)
+    };
+}
 
+/// Defines the application-specific interrupt handler section of the interrupt vector table.
+///
+/// The interrupt vectors specified in the arguments are placed sequentially after the reset and
+/// exception vectors, starting from index `INTERRUPT_START`. The rest of the table is filled
+/// with `InterruptVector::DEFAULT`.
+///
+/// ```
+/// interrupt_vectors![
+///     InterruptVector(MyHandler1, 0), // int INTERRUPT_START+0
+///     InterruptVector(MyHandler2, 0), // int INTERRUPT_START+1
+///     InterruptVector(MyHandler3, 0), // int INTERRUPT_START+2
+/// ];
+/// ```
+#[cfg(feature = "interrupts")]
 #[macro_export]
 macro_rules! interrupt_vectors {
     ($($elems:expr),* $(,)?) => {
@@ -23,11 +55,7 @@ macro_rules! interrupt_vectors {
                 prefix: [$crate::InterruptVector; M],
             ) -> [$crate::InterruptVector; N] {
                 assert!(M <= N, "Prefix length cannot exceed array length");
-
-                unsafe extern "cdm-isr" {
-                    fn InterruptHandler();
-                }
-                let mut arr = [$crate::InterruptVector(InterruptHandler, 0); N];
+                let mut arr = [$crate::InterruptVector::DEFAULT; N];
                 let mut i: usize = 0;
                 while i < M {
                     arr[i] = prefix[i];
@@ -40,6 +68,10 @@ macro_rules! interrupt_vectors {
         };
     };
 }
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+struct ExceptionVector(pub unsafe extern "C" fn() -> !, pub u16);
 
 // The initialization code
 core::arch::global_asm!(
@@ -94,9 +126,5 @@ static __EXCEPTIONS: [ExceptionVector; EXCEPTION_COUNT] = [
 #[used]
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".ivt.interrupts")]
-static __INTERRUPTS: [InterruptVector; INTERRUPT_COUNT] = [{
-    unsafe extern "cdm-isr" {
-        fn InterruptHandler();
-    }
-    InterruptVector(InterruptHandler, 0)
-}; INTERRUPT_COUNT];
+static __INTERRUPTS: [InterruptVector; INTERRUPT_COUNT] =
+    [InterruptVector::DEFAULT; INTERRUPT_COUNT];
