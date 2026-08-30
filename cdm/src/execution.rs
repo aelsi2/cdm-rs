011ca763a8329588f8e630e0f1fc8968519a2b0a
aelsi2 <eliseevandrey2005@gmail.com>
2026-08-30 20:15:09 +0700

    Improve docs for cdm::execution and cdm::interrupt

--- cdm/src/execution.rs
+++ cdm/src/execution.rs
@@ -2,13 +2,24 @@
 
 use core::arch::asm;
 
-/// Stops the clock, putting the processor into the `HALTED` state.
+/// Puts the processor into the `HALTED` state.
+///
+/// The processor stops executing instructions and handling interrupt requests.
+/// The only way to make it run again is to perform a hard reset.
 #[inline(always)]
 pub fn halt() -> ! {
     unsafe { asm!("halt", options(nostack, noreturn)) }
 }
 
-/// Performs a soft reset, fetching interrupt vector 0.
+/// Performs a soft reset, fetching the reset vector from the IVT.
+///
+/// # Safety
+/// When using `cdm-rt` with the von Neumann architecture, the processor keeps the old 
+/// `.data` and `.bss` sections after the soft reset, so calling this function may lead 
+/// to undefined behavior.
+///
+/// This function is safe to call when using `cdm-rt` with the Harvard architecture, because 
+/// the aforementioned sections are initialized at startup.
 #[inline(always)]
 pub unsafe fn reset() -> ! {
     unsafe { asm!("reset", options(nostack, noreturn)) }
--- cdm/src/interrupt.rs
+++ cdm/src/interrupt.rs
@@ -2,20 +2,28 @@
 
 use core::arch::asm;
 
-/// Enables all interrupts.
+/// Enables hardware interrupts.
+///
+/// # Safety
+/// Enabling hardware interrupts will end the current critical section (if there is one).
+/// This may lead to a race condition if an interrupt is triggered.
 #[inline(always)]
 pub unsafe fn enable() {
     unsafe { asm!("ei", options(nostack)) };
 }
 
-/// Disables all interrupts.
+/// Disables hardware interrupts.
 #[inline(always)]
 pub fn disable() {
     unsafe { asm!("di", options(nostack)) };
 }
 
-/// Stops the clock until an interrupt request is received,
-/// putting the processor into the `WAITING` state.
+/// Puts the processor into the `WAITING` state.
+///
+/// The processor stops executing instructions until an interrupt request is received.
+/// If the processor is waiting with hardware interrupts disabled, an IRQ signal will still
+/// wake it up, resuming execution after the `wait` instruction without granting the interrupt
+/// request.
 #[inline(always)]
 pub fn wait() {
     unsafe { asm!("wait", options(nostack, preserves_flags)) };
@@ -24,6 +32,14 @@ pub fn wait() {
 /// Triggers a software interrupt with the number `V`.
 ///
 /// `V` must be in the range [0; 63].
+///
+/// # Safety
+/// Triggering interrupt 0 has the same effect as performing a soft reset (except that the previous
+/// values of PC and PS are pushed to the stack), which may cause undefined behavior. 
+/// See [`cdm::execution::reset`](function@crate::execution::reset).
+///
+/// *Note: triggering other interrupt vectors should not cause undefined behavior by itself, but may cause 
+/// bugs if the handlers are not prepared for this case.*
 #[inline(always)]
 pub unsafe fn trigger<const V: u8>() {
     const {
